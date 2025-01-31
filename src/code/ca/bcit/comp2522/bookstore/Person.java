d8f492a4f326dae6256c2c2b38d781864b898518
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-31 14:56:42 -0800

    feat: implemented comparable, printable, reversible

--- src/code/ca/bcit/comp2522/bookstore/Person.java
+++ src/code/ca/bcit/comp2522/bookstore/Person.java
@@ -1,5 +1,53 @@
 package ca.bcit.comp2522.bookstore;
 
-public class Person
+// Nate: Person, Date(implement {Printable}, author
+
+public class Person implements Comparable,
+                               Printable,
+                               Reversible
 {
+    final Date dateOfBirth;
+    final Date dateOfDeath;
+    final Name name;
+
+    public Person(final Date dateOfBirth,
+                  final Date dateOfDeath,
+                  final Name name)
+    {
+        validateDateOfBirth(dateOfBirth);
+        validateDateOfDeath(dateOfDeath);
+        validateName(name);
+
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.name        = name;
+    }
+
+    @Override
+    public void display()
+    {
+
+    }
+
+    @Override
+    public void backwards()
+    {
+
+    }
+
+    @Override
+    public int compareTo(final Object o)
+    {
+        return 0;
+    }
+
+    private static void validateDate(final Date date)
+    {
+
+    }
+
+    private static void validateName(final Name name)
+    {
+
+    }
 }
