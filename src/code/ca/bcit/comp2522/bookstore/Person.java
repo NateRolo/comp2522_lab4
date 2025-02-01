ca92331225ad0616f7fa88cef8debb18789392d5
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-31 16:14:21 -0800

    feat: write display to display person name, birth date, and death date(if person died).

--- src/code/ca/bcit/comp2522/bookstore/Person.java
+++ src/code/ca/bcit/comp2522/bookstore/Person.java
@@ -47,13 +47,58 @@ public class Person implements Comparable,
     @Override
     public void display()
     {
+        final StringBuilder builder;
+        final String details;
+        final String dateOfBirth;
+        final String dateOfDeath;
+        final String name;
 
+        final boolean personIsAlive;
+
+        personIsAlive = (this.dateOfDeath == null);
+        dateOfBirth = this.dateOfBirth.getFullDate();
+        name = this.name.getFullName();
+
+        if(personIsAlive)
+        {
+            dateOfDeath = "Person is still alive.";
+        }
+        else
+        {
+            dateOfDeath = this.dateOfDeath.getFullDate();
+        }
+
+        builder = new StringBuilder();
+
+        builder.append(name)
+                .append("\n")
+                .append("Date of birth: ")
+                .append(dateOfBirth)
+                .append("\n")
+                .append("Date of death: ")
+                .append(dateOfDeath)
+                .append("\n");
+
+        details = builder.toString();
+
+        System.out.println(details);
     }
 
     @Override
     public void backwards()
     {
+        final StringBuilder builder;
+        final String reversedName;
 
+        builder = new StringBuilder();
+
+        builder.append(this.name.getFullName());
+
+        builder.reverse();
+
+        reversedName = builder.toString();
+
+        System.out.println(reversedName);
     }
 
     @Override
