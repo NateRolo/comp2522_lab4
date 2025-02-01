7f29e1b4f7aefc61fc94d092660f8cd172d939a8
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-31 16:25:56 -0800

    feat: write constructor and validate genre method

--- src/code/ca/bcit/comp2522/bookstore/Author.java
+++ src/code/ca/bcit/comp2522/bookstore/Author.java
@@ -1,5 +1,50 @@
 package ca.bcit.comp2522.bookstore;
 
-public class Author
+public class Author extends Person
 {
+    private static final int MAX_GENRE_NAME_LENGTH = 30;
+
+    private String genre;
+
+    public Author(final Date dateOfBirth,
+                  final Date dateOfDeath,
+                  final Name name,
+                  final String genre)
+    {
+        super(dateOfBirth,
+              dateOfDeath,
+              name);
+
+        validateGenre(genre);
+
+        this.genre = genre;
+    }
+
+    private static void validateGenre(final String genre)
+    {
+        final boolean genreNameExceedsMaxLength;
+        final int genreNameLength;
+
+        if(genre == null)
+        {
+            throw new IllegalArgumentException("Genre cannot be null.");
+        }
+
+        if(genre.isBlank())
+        {
+            throw new IllegalArgumentException("Genre cannot be blank.");
+        }
+
+        genreNameLength = genre.length();
+
+        genreNameExceedsMaxLength = genreNameLength > MAX_GENRE_NAME_LENGTH;
+
+        if(genreNameExceedsMaxLength)
+        {
+            throw new IllegalArgumentException("Genre name too long: " +
+                                               genreNameLength);
+        }
+    }
+
+
 }
--- src/code/ca/bcit/comp2522/bookstore/Person.java
+++ src/code/ca/bcit/comp2522/bookstore/Person.java
@@ -1,5 +1,138 @@
 package ca.bcit.comp2522.bookstore;
 
-public class Person
+// Nate: Person, Date(implement {Printable}, author
+
+public class Person implements Comparable,
+                               Printable,
+                               Reversible
 {
+    private final Date dateOfBirth;
+    private final Date dateOfDeath;
+    private final Name name;
+
+    public Person(final Date dateOfBirth,
+                  final Date dateOfDeath,
+                  final Name name)
+    {
+        validateDateOfBirth(dateOfBirth);
+
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        this.name        = name;
+    }
+
+    public Person(final Date dateOfBirth,
+                  final Name name)
+    {
+        this(dateOfBirth,
+             null,
+             name);
+    }
+
+    public final Date getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    public final Date getDateOfDeath()
+    {
+        return dateOfDeath;
+    }
+
+    public final Name getName()
+    {
+        return name;
+    }
+
+    @Override
+    public void display()
+    {
+        final StringBuilder builder;
+        final String details;
+        final String dateOfBirth;
+        final String dateOfDeath;
+        final String name;
+
+        final boolean personIsAlive;
+
+        personIsAlive = (this.dateOfDeath == null);
+        dateOfBirth = this.dateOfBirth.getFullDate();
+        name = this.name.getFullName();
+
+        if(personIsAlive)
+        {
+            dateOfDeath = "Person is still alive.";
+        }
+        else
+        {
+            dateOfDeath = this.dateOfDeath.getFullDate();
+        }
+
+        builder = new StringBuilder();
+
+        builder.append(name)
+                .append("\n")
+                .append("Date of birth: ")
+                .append(dateOfBirth)
+                .append("\n")
+                .append("Date of death: ")
+                .append(dateOfDeath)
+                .append("\n");
+
+        details = builder.toString();
+
+        System.out.println(details);
+    }
+
+    @Override
+    public void backwards()
+    {
+        final StringBuilder builder;
+        final String reversedName;
+
+        builder = new StringBuilder();
+
+        builder.append(this.name.getFullName());
+
+        builder.reverse();
+
+        reversedName = builder.toString();
+
+        System.out.println(reversedName);
+    }
+
+    @Override
+    public int compareTo(final Object o)
+    {
+        final boolean objectIsNotAPerson;
+        final boolean thisPersonIsOlder;
+        final int ageDifference;
+
+        if(o == null)
+        {
+            throw new IllegalArgumentException("Cannot compare to null object.");
+        }
+
+        objectIsNotAPerson = !(this.getClass().equals(o.getClass()));
+
+        if(objectIsNotAPerson)
+        {
+            throw new IllegalArgumentException("Cannot compare this person" +
+                                               " to non-person object.");
+        }
+
+        ageDifference = this.dateOfBirth.getYear() -
+                        ((Person)o).getDateOfBirth().getYear();
+
+        return ageDifference;
+    }
+
+    private static void validateDateOfBirth(final Date dateOfBirth)
+    {
+        if(dateOfBirth == null)
+        {
+            throw new IllegalArgumentException("Date of birth cannot be null,");
+        }
+    }
+
 }
