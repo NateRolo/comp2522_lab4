bb1daf57719d20f8013e3b3e0d52ac4952546a31
Lucas Liu <lucasqhliu@gmail.com>
2025-02-01 16:12:20 -0800

    feat: created the biography and autobiography class

--- src/code/ca/bcit/comp2522/bookstore/Biography.java
+++ src/code/ca/bcit/comp2522/bookstore/Biography.java
@@ -1,5 +1,46 @@
 package ca.bcit.comp2522.bookstore;
 
-public class Biography
+public class Biography extends Book
+                        implements Printable
 {
+    private final Person subject;
+
+    public Biography(final String title,
+                     final int yearPublished,
+                     final Author author,
+                     final Person subject)
+    {
+        super(title, yearPublished, author);
+
+        validateSubject(subject);
+
+        this.subject = subject;
+    }
+
+    @Override
+    public void display() {
+        //to be added once merged.
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        //to be added once merged with person class
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        //to be added once equals is implemented.
+        return 0;
+    }
+
+    private static void validateSubject(final Person subject)
+    {
+        if(subject == null)
+        {
+            throw new IllegalArgumentException("invalid subject");
+        }
+    }
 }
